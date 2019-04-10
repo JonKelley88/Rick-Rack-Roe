06fb455643185ebd424d764ccc7eb1dfb2f1e7af
Jon Kelley <kelley@josephs-mbp-2.attlocal.net>
2019-04-10 13:52:35 -0500

    docker

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
 
+const PORT = 1111;
 const app = express();
 
 app.use('/public', express.static('dist'));
@@ -9,4 +10,4 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.render('home'));
 
-app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
